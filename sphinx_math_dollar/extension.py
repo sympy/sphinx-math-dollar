021ddd976c2eb884552ad0065ebf925ef01ea59f
Aaron Meurer <asmeurer@gmail.com>
2019-09-13 16:27:25 -0600

    Get the extension basically working
    One issue is that it uses rawsource, because otherwise the backslashes are
    stripped out. But this also undoes other things that RST does, like replacing
    quotes with pretty quotes. I'm not sure yet how to fix this.

--- sphinx_math_dollar/extension.py
+++ sphinx_math_dollar/extension.py
@@ -1,13 +1,30 @@
 from .math_dollar import split_dollars
 
-from docutils.nodes import NodeVisitor
+from docutils.nodes import GenericNodeVisitor, Text, math, paragraph
 
-class MathDollarReplacer(NodeVisitor):
+class MathDollarReplacer(GenericNodeVisitor):
+    def default_visit(self, node):
+        return node
+
     def visit_Text(self, node):
-        _data = split_dollars(node.astext())
+        if not isinstance(node.parent, paragraph):
+            return
+        data = split_dollars(node.rawsource)
+        nodes = []
+        has_math = False
+        for typ, text in data:
+            if typ == "math":
+                has_math = True
+                nodes.append(math(text, Text(text)))
+            elif typ == "text":
+                nodes.append(Text(text))
+            else:
+                raise ValueError("Unrecognized type from split_dollars %r" % typ)
+        if has_math:
+            node.parent.replace(node, nodes)
 
 def process_doctree(app, doctree):
-    pass
+    doctree.walk(MathDollarReplacer(doctree))
 
 def setup(app):
     app.connect("doctree-read", process_doctree)

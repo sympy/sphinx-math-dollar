621aec3feb6769e6daac3f8c3e48ce5d5c59b6dc
Aaron Meurer <asmeurer@gmail.com>
2019-09-13 15:22:38 -0600

    Update the docstring of split_dollars()

--- sphinx_math_dollar/math_dollar.py
+++ sphinx_math_dollar/math_dollar.py
@@ -2,24 +2,38 @@ import re
 
 def split_dollars(text):
     r"""
-    Replace dollar signs with backticks.
+    Split text into text and math segments.
 
-    More precisely, do a regular expression search.  Replace a plain
-    dollar sign ($) by a backtick (`).  Replace an escaped dollar sign
-    (\$) by a dollar sign ($).  Don't change a dollar sign preceded or
-    followed by a backtick (`$ or $`), because of strings like
-    "``$HOME``".  Don't make any changes on lines starting with
-    spaces, because those are indented and hence part of a block of
-    code or examples.
+    Returns a list of tuples ``(type, text)``, where ``type`` is either
+    ``"text"`` or ``"math"`` and ``text`` is the text.
+
+    Example:
+
+    >>> split_dollars(r"The functions $\sin(x)$ and $\cos(x)$.")
+    [('text', 'The functions '), ('math', '\\sin(x)'), ('text', ' and '),
+    ('math', '\\cos(x)'), ('text', '.')]
+
+    More precisely, do a regular expression search.  To match as math, the
+    first character after the first $ should not be a space. This is to avoid
+    false positives with things like
+
+    $ cd ~
+    $ ls
+
+    Escaped dollars (\$) are also not matched as math delimiters, however all
+    escaped dollars are replaced with normal dollars in the final output.
+
+    Math is allowed to be split across multiple lines, as its assumed the
+    dollars will appear in places like docstrings where line wrapping is
+    desired.
 
     This also doesn't replaces dollar signs enclosed in curly braces,
     to avoid nested math environments, such as ::
 
       $f(n) = 0 \text{ if $n$ is prime}$
 
-    Thus the above line would get changed to
+    Thus the above line would get matched fully as math.
 
-      :math:`f(n) = 0 \text{ if $n$ is prime}`
     """
     # This searches for "$blah$" inside a pair of curly braces --
     # don't change these, since they're probably coming from a nested

9cc3e9af9f342a7e2a2c31aeeac70a6d2c4463ea
Aaron Meurer <asmeurer@gmail.com>
2019-09-17 14:15:56 -0600

    Export NODE_BLACKLIST

--- sphinx_math_dollar/__init__.py
+++ sphinx_math_dollar/__init__.py
@@ -1,7 +1,7 @@
 from .math_dollar import split_dollars
-from .extension import setup
+from .extension import setup, NODE_BLACKLIST
 
-__all__ = ['split_dollars', 'setup']
+__all__ = ['split_dollars', 'setup', 'NODE_BLACKLIST']
 
 from ._version import get_versions
 __version__ = get_versions()['version']

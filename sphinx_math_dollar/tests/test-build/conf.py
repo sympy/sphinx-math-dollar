cd1bb73e4d8e5a418aa2d34bafcce9e38d88418e
Aaron Meurer <asmeurer@gmail.com>
2019-09-17 13:45:44 -0600

    Forgot to actually enable the extension in the test build

--- sphinx_math_dollar/tests/test-build/conf.py
+++ sphinx_math_dollar/tests/test-build/conf.py
@@ -28,6 +28,7 @@ author = 'author'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_math_dollar',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
